e21ab115c4719e059ba74a3cdd1313e232244f9e
jorge <luizfonseca830@gmail.com>
2021-03-28 16:09:11 -0500

    tradução e  fixar barra do menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,11 +42,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Logar') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Cadastre-se') }}</a>
                                 </li>
                             @endif
                         @else
@@ -59,7 +59,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Sair') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
--- resources/views/layouts/weelcome/menu.blade.php
+++ resources/views/layouts/weelcome/menu.blade.php
@@ -1,67 +1,75 @@
 <header class="main_menu home_menu">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-12">
-                <nav class="navbar navbar-expand-lg navbar-light">
+    
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Acre Saude
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                    <a class="navbar-brand" href="{{route('inicio')}}"> <img
-                            src="{{asset('images/logoprincipal.png')}}"
-                            alt="logo"> </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <li class="collapse navbar-collapse main-menu-item justify-content-center"
-                         id="navbarSupportedContent">
-                        <ul class="navbar-nav align-items-center">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('inicio') }}">{{ __('Inicio') }}</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Doutores</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Logar') }}</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="dropdown-item" href="#">Serviços</a>
-                            </li>
+                        @endif
+                    @else
+
+                        <li class="nav-item dropdown">
+                            @if(is_null(auth()->user()))
+                                <a class="btn_2 d-none d-lg-block" href="{{route('login')}}">Login</a>
+                        @else
+
                             <li class="nav-item dropdown">
-                                @if(is_null(auth()->user()))
-                                    <a class="btn_2 d-none d-lg-block" href="{{route('login')}}">Login</a>
-                                @else
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Minha Conta
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    @if (auth()->user()->pessoa->tipoUsuario->nome == 'Administrador')
+                                        <a class="dropdown-item" href="{{route('home.dashboard')}}">Área
+                                            Adiministrativa</a>
+                                    @endif
+                                    @if(auth()->user()->pessoa->tipoUsuario->nome == 'Doutor')
+                                        <a class="dropdown-item" href="{{route('agenda.index')}}">Agenda Pessoal</a>
+                                    @endif
 
-                                        <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                Minha Conta
-                                            </a>
-                                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                @if (auth()->user()->pessoa->tipoUsuario->nome == 'Administrador')
-                                                    <a class="dropdown-item" href="{{route('home.dashboard')}}">Área Adiministrativa</a>
-                                                @endif
-                                                @if(auth()->user()->pessoa->tipoUsuario->nome == 'Doutor')
-                                                    <a class="dropdown-item" href="{{route('agenda.index')}}">Agenda Pessoal</a>
-                                                @endif
-
-                                                @if(auth()->user()->pessoa->tipoUsuario->nome == 'Doutor')
-                                                    <a class="dropdown-item" href="{{route('medico.consultas.index')}}">Consultas
-                                                        Marcadas</a>
-                                                @else
-                                                    <a class="dropdown-item" href="{{route('paciente.consultas.index')}}">Consultas
-                                                        Marcadas</a>
-                                                @endif
-                                                <a class="dropdown-item" href="{{route('minhascompras.index')}}">Minhas Compras</a>
-                                                <form method="post" action="{{route('sair')}}">
-                                                    @csrf
-                                                    <input type="submit" class="dropdown-item" value="Sair">
-                                                </form>
-                                            </div>
-                                        </li>
-
-                                @endif
+                                    @if(auth()->user()->pessoa->tipoUsuario->nome == 'Doutor')
+                                        <a class="dropdown-item" href="{{route('medico.consultas.index')}}">Consultas
+                                            Marcadas</a>
+                                    @else
+                                        <a class="dropdown-item" href="{{route('paciente.consultas.index')}}">Consultas
+                                            Marcadas</a>
+                                    @endif
+                                    <a class="dropdown-item" href="{{route('minhascompras.index')}}">Minhas Compras</a>
+                                    <form method="post" action="{{route('sair')}}">
+                                        @csrf
+                                        <input type="submit" class="dropdown-item" value="Sair">
+                                    </form>
+                                </div>
                             </li>
-                        </ul>
-                    </div>
 
-                </nav>
+                            @endif
+                            </li>
+                        @endguest
+                </ul>
             </div>
         </div>
-    </div>
+    </nav>
 </header>
